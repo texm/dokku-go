521430a7550c138e8cc4fc0637f8de46f136babc
texm <texmoran2@gmail.com>
2022-07-11 21:19:11 +0800

    list plugins

--- plugin.go
+++ plugin.go
@@ -1,22 +1,38 @@
 package dokku
 
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
 type pluginManager interface {
-	SetPluginEnabled(plugin string, enabled bool) error
+	/*
+		EnablePlugin(plugin string) error
+		DisablePlugin(plugin string) error
 
-	InstallPlugin(options PluginInstallOptions) error
-	InstallPluginDependencies(coreOnly bool) error
-	UninstallPlugin(plugin string) error
-	UpdatePlugin(plugin string) error
-	UpdatePlugins() error
+		CheckPluginInstalled(plugin string) (bool, error)
+		InstallPlugin(options PluginInstallOptions) error
+		InstallPluginDependencies() error
+		UninstallPlugin(plugin string) error
+		UpdatePlugin(plugin string) error
+		UpdatePlugins() error
 
-	ListPlugins() ([]string, error)
+		TriggerPluginHook(hookArgs []string) error
+	*/
 
-	TriggerPluginHook(hookArgs []string) error
+	ListPlugins() ([]PluginInfo, error)
 }
 
-type PluginInstallOptions struct{}
+type PluginInfo struct {
+	Name        string
+	Version     string
+	Enabled     bool
+	Description string
+}
 
 const (
+	pluginInstalledCmd           = "plugin:installed %s"
 	pluginDisableCmd             = "plugin:disable %s"
 	pluginEnableCmd              = "plugin:enable %s"
 	pluginInstallGitCmd          = "plugin:install <git-url> --committish <tag|branch|commit> --name <custom-plugin-name>"
@@ -27,9 +43,45 @@ const (
 	pluginUpdateCmd              = "plugin:update %s %s"
 )
 
-func (c *DefaultClient) SetPluginEnabled(plugin string, enabled bool) error {
-	//TODO implement me
-	panic("implement me")
+var multipleWhitespaceRe = regexp.MustCompile("\\s+")
+
+func (c *DefaultClient) ListPlugins() ([]PluginInfo, error) {
+	out, err := c.Exec(pluginListCmd)
+	lines := strings.Split(out, "\n")
+	plugins := make([]PluginInfo, len(lines))
+	for i, line := range lines {
+		line = strings.TrimSpace(line)
+		cols := multipleWhitespaceRe.Split(line, 4)
+		if len(cols) < 4 {
+			return nil, fmt.Errorf("error parsing plugin list line: %s", line)
+		}
+
+		plugins[i] = PluginInfo{
+			Name:        cols[0],
+			Version:     cols[1],
+			Enabled:     cols[2] == "enabled",
+			Description: cols[3],
+		}
+	}
+
+	return plugins, err
+}
+
+/*
+func (c *DefaultClient) CheckPluginInstalled(plugin string) (bool, error) {
+
+}
+
+func (c *DefaultClient) EnablePlugin(plugin string) error {
+	cmd := fmt.Sprintf(pluginEnableCmd, plugin)
+	_, err := c.Exec(cmd)
+	return err
+}
+
+func (c *DefaultClient) DisablePlugin(plugin string) error {
+	cmd := fmt.Sprintf(pluginDisableCmd, plugin)
+	_, err := c.Exec(cmd)
+	return err
 }
 
 func (c *DefaultClient) InstallPlugin(options PluginInstallOptions) error {
@@ -37,27 +89,24 @@ func (c *DefaultClient) InstallPlugin(options PluginInstallOptions) error {
 	panic("implement me")
 }
 
-func (c *DefaultClient) InstallPluginDependencies(coreOnly bool) error {
-	//TODO implement me
-	panic("implement me")
+func (c *DefaultClient) InstallPluginDependencies() error {
+	_, err := c.Exec(pluginInstallDependenciesCmd)
+	return err
 }
 
 func (c *DefaultClient) UninstallPlugin(plugin string) error {
-	//TODO implement me
-	panic("implement me")
+	cmd := fmt.Sprintf(pluginUninstallCmd, plugin)
+	_, err := c.Exec(cmd)
+	return err
 }
 
 func (c *DefaultClient) UpdatePlugin(plugin string) error {
-	//TODO implement me
-	panic("implement me")
+	cmd := fmt.Sprintf(pluginUpdateCmd, plugin)
+	_, err := c.Exec(cmd)
+	return err
 }
 
 func (c *DefaultClient) UpdatePlugins() error {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (c *DefaultClient) ListPlugins() ([]string, error) {
 	//TODO implement me
 	panic("implement me")
 }
@@ -66,3 +115,4 @@ func (c *DefaultClient) TriggerPluginHook(hookArgs []string) error {
 	//TODO implement me
 	panic("implement me")
 }
+*/
--- plugin_test.go
+++ plugin_test.go
@@ -1,1 +1,10 @@
 package dokku
+
+func (s *DokkuTestSuite) TestListPlugins() {
+	r := s.Require()
+
+	plugins, err := s.Client.ListPlugins()
+	r.NoError(err)
+	r.NotEmpty(plugins)
+	r.NotEmpty(plugins[0])
+}
